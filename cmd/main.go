e68c4a83acbb8c95c4d091f557135e412a966cd6
Siddharth More <siddimore@gmail.com>
2026-04-22 22:44:06 -0700

    Add files via upload

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"distributed_lock_demo/internal"
-	"distributed_lock_demo/rideshare"
 	"log"
 	"net/http"
 )
@@ -11,7 +10,7 @@ func main() {
 	rclient := internal.NewRedisClient()
 
 	http.HandleFunc("/accept", func(w http.ResponseWriter, r *http.Request) {
-		rideshare.AcceptRide(w, r, rclient)
+		internal.AcceptRide(w, r, rclient)
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
--- internal/model.go
+++ internal/model.go
@@ -1,5 +1,6 @@
 package internal
 
+// Ride represents a ride request and its current assignment state.
 type Ride struct {
 	ID       string
 	DriverID string
